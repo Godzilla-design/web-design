70185ca4ffb7cd7531b07d80334c5ecbeafd9cca
Godzilla-design <chinonsoarinze76@gmail.com>
2025-07-31 22:34:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let userAge = prompt("Please enter your age:");
+if (userAge < 18) {
+    alert("sorry you are too young to drive this car.powering off.");
+}
+ else if (userAge = 18) {
+    alert("congratulations on your first year of driving.enjoy the ride.");
+}
+else if (userAge > 18) {
+    alert("powering on enjoy the ride.");
+}
